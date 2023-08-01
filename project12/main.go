d73d966f6e66c13b53ae289746baba693bbd90b6
Thales Faggiano <thalesfaggiano@gmail.com>
2023-08-01 06:23:50 -0300

    feat(cep): Adding a get http request

--- project12/main.go
+++ project12/main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 	for _, cep := range os.Args[1:] {
+		req, err := http.Get(cep)
+		if err != nil {
+			fmt.Printf("Error: %s", err)
+		}
+		defer req.Body.Close()
+
 		println(cep)
+		println(req)
 	}
 }
