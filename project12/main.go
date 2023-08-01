ef9de9bf84cd7a445ce0a637c2dc407b0b386ad0
Thales Faggiano <thalesfaggiano@gmail.com>
2023-08-01 07:08:36 -0300

    feat(cep): Adding ViaCEP Request

--- project12/main.go
+++ project12/main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
 	for _, cep := range os.Args[1:] {
-		req, err := http.Get(cep)
+		req, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
 		if err != nil {
-			fmt.Printf("Error: %s", err)
+			fmt.Fprintf(os.Stderr, "Error: %s", err)
 		}
 		defer req.Body.Close()
 
-		println(cep)
+		println("CEP = " + cep)
 		println(req)
+
+		res, err := io.ReadAll(req.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %s", err)
+		}
+		fmt.Fprintf(os.Stdout, "%s\n", string(res))
+
+		// bash troubleshooting:
+		// diff <(go run project12/main.go 04160001) <(go run project12/main.go 04160000)
 	}
 }
