6b5bd0bb87d8d517278c8969a353c88e85a3ce21
Thales Faggiano <thalesfaggiano@gmail.com>
2023-08-01 07:20:00 -0300

    feat(cep): Adding data struct and marshall json

--- project12/main.go
+++ project12/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
 
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
+
 func main() {
 	for _, cep := range os.Args[1:] {
 		req, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %s", err)
+			fmt.Fprintf(os.Stderr, "Request Error: %s", err)
 		}
 		defer req.Body.Close()
 
@@ -20,11 +34,14 @@ func main() {
 
 		res, err := io.ReadAll(req.Body)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error: %s", err)
+			fmt.Fprintf(os.Stderr, "Response Error: %s", err)
 		}
 		fmt.Fprintf(os.Stdout, "%s\n", string(res))
-
-		// bash troubleshooting:
-		// diff <(go run project12/main.go 04160001) <(go run project12/main.go 04160000)
+		var data ViaCEP
+		err = json.Unmarshal(res, &data)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unmarshal Error: %s", err)
+		}
+		fmt.Println(data)
 	}
 }
